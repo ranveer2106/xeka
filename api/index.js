a4ff1529a1b5c4ae936cb80e59d2978d457881ce
Ranvir Singh Bhatti <ranvirsingh.bhatti2020@vitstudent.ac.in>
2023-01-07 16:14:50 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const app = express();
+
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config();
+// mongoose.set('strictQuery', true);
+// mongoose.connect(process.env["Mongo_url"], {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     // useCreateIndex: true,
+// })
+//     .then(console.log("connected to database"))
+//     .catch((err) => console.log(err))
+// console.log(process.env["Mongo_url"]);
+
+// app.get
+// console.log("fuck");
+
+app.use("/lora", (req, res) => {
+    console.log("backen[fvkdflkvndfl] is running");
+})
+
+app.listen("5000", () => {
+    console.log("Backend is running")
+})
