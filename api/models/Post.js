1d27c354fa75354dc3c54be043243dd7bea3998b
Ranvir Singh Bhatti <ranvirsingh.bhatti2020@vitstudent.ac.in>
2023-01-08 20:21:18 +0530

    routes

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,7 +1,23 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    categories: {
+        type: Array,
+        required: true,
+    }
 },
     { timestamps: true }
 )
---
+++ api/routes/auth.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const User = require("../models/User")
+
+// Register
+
+router.post("./register", async (req, res) => {
+
+})
