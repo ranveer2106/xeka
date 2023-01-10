f58d6e47829f4aabdb54334892f45ae004373645
Ranvir Singh Bhatti <ranvirsingh.bhatti2020@vitstudent.ac.in>
2023-01-10 23:18:43 +0530

    auth

--- api/index.js
+++ api/index.js
@@ -2,23 +2,28 @@ const express = require("express")
 const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
+
+app.use(express.json());
 
 dotenv.config();
-// mongoose.set('strictQuery', true);
-// mongoose.connect(process.env["Mongo_url"], {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     // useCreateIndex: true,
-// })
-//     .then(console.log("connected to database"))
-//     .catch((err) => console.log(err))
-// console.log(process.env["Mongo_url"]);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env["Mongo_url"], {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+})
+    .then(console.log("connected to database"))
+    .catch((err) => console.log(err))
+console.log(process.env["Mongo_url"]);
 
 // app.get
 // console.log("fuck");
 
-app.use("/lora", (req, res) => {
-    console.log("backen[fvkdflkvndfl] is running");
+app.use("/api/auth", authRoute)
+
+app.use("/home", (req, res) => {
+    console.log("backend is running in home");
 })
 
 app.listen("5000", () => {
