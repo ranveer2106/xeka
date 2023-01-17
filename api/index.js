2e729b21f823551b98a4e6d53665005acc965bee
Ranvir Singh Bhatti <ranvirsingh.bhatti2020@vitstudent.ac.in>
2023-01-17 17:51:58 +0530

    git ignore

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
-// console.log("Hello World")
-
 const express = require("express")
-
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+}).then(console.log("Connected to MongoDB")).catch(err => console.log(err))
+
+// console.log(process.env["MONGO_URL"]);
+
+dotenv.config();
 
 app.get("/home", (req, res) => {
     console.log("hey this is home url");
